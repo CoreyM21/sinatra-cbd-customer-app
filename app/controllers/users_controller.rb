44ee79b37ef22654e7c1c3a4642dc77101e0c21a
CoreyM21 <coreyjmartin2@gmail.com>
2020-12-12 15:11:31 -0500

    added user show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,14 +28,47 @@ class UsersController < ApplicationController
     end
 
     #what routes do I need for signup?
+    # this routes's job is to render the signup form
     get '/signup' do 
+        # erb (render) a view 
+        erb :signup 
+    end
+
+    post '/users' do
+        # here is where we will create a new user and persisit the new user to the DB
+        # params will look like this: 
+        # {
+        #     "name"=>"Corey", 
+        #     "email"=>"corey@corey.com", 
+        #     "password"=>"password"
+        # }
+        # Io nly want to persist a user that has a name email, AND password
+        if params[:name] != "" && params[:email] != "" && params[:password] != ""
+            # valid input
+            @user = User.create(params)
+            # where do I go now?
+            # go to user show page
+            redirect '/users/#{@user.id}'
+            erb :'/users/show'
+        else 
+            # not valid input
+            # it would be better to include a message to the user telling them what is wrong
+            redirect '/signup'
+        end 
 
     end
 
     # user SHOW route 
     get '/users/:id' do 
-        "This will be the user show route"
+        # what do I need to do first?
+        @user = User.find_by(id: params[:id])
+        session[:user_id] = @user.id #actually logging user in
+        erb :'/users/show'
     end
 
+    get '/logout' do 
+        session.clear
+        redirect '/'
+    end
 
 end
